b273b5a1e4a72e8c38ebe4975eb0234c2ee70cff
Aakash <aakash21696@gmail.com>
2023-01-31 23:28:49 -0800

    adding spark code to find busy time slots of invitees

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ on their availability.
 It uses [Flowframe](https://github.com/owenarden/flowframe), which is a Scala compiler plugin that performs fine-grained
  information flow analysis on code to detect privacy violations.
 To do this, FlowFrame enforces information flow control policies in Scala Spark applications, specified by @Policy annotations
- that specify "purpose limitations". Refer to the Flowframe documentation to learn more.  
+ that specify purpose limitations. Refer to the Flowframe documentation to learn more.  
 
 Safe scheduler uses policies to ensure that only relevant information (like invitees, start and end time of their
  respective events, etc) is accessible when finding a suitable meeting time - as opposed to event details which is
--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -15,14 +15,23 @@ object Driver {
     val calendarTable = Calendar(spark)
     calendarTable.show()
     val eventTable = Event(spark)
-    val todayString = "2023-01-27 18:00:00"
-    val todayTimestamp = Timestamp.valueOf(todayString)
-    print("Event table")
-    eventTable.filter(event => event.calendar_id == 1 || event.calendar_id == 2)
-        .filter(event => event.start_time.before(todayTimestamp)).show()
+    eventTable.show()
+    findSuitableTime(calendarTable, userTable, eventTable, spark)
   }
 
-  // should return a start time for the meeting of the given 'duration' between all the 'users'
-  def findSuitableTime(calendar: Dataset[Calendar], users: Dataset[User], event: Dataset[Event], duration: Int): Unit = {
+  // returns all possible 30-minute meeting slots with no conflict between invitees
+  def findSuitableTime(calendar: Dataset[Calendar], users: Dataset[User], event: Dataset[Event], sparkSession: SparkSession): Unit = {
+    val joinCondition = users.col("id") === calendar.col("user_id")
+    val inviteeCalendars = calendar.join(users, joinCondition, "left_semi")
+    inviteeCalendars.show()
+    val eventIDs = event.join(inviteeCalendars, event.col("calendar_id") === inviteeCalendars.col("id"),
+      "left_semi")
+    // i need to find 30 minute time slots that are available for all the invitees (starting from 9 am and between 6 pm)
+    // to do this, I will first look at busy slots. say, aakash is busy from 6 to 7 and aditya is busy from 5-7
+    // i need to exclude the time slot (5-530, 530-6, 6-630, 630 - 7) from all time slots starting from 9:00 and ending at 21:00 (for now)
+    // return all of these time slots in an array.
+    val busyTimeSlots = eventIDs.select("start_time", "end_time").collectAsList()
+    println("Busy time slots for all invitees ")
+    println(busyTimeSlots)
   }
 }
